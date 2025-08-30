2c3d51dd1de41bbffdd82659bb75f848223f6cc4
Omur56 <omur199624@gmail.com>
2025-08-31 01:21:27 +0400

    refres

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,10 +1,27 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true, unique: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   phone: { type: String, required: true }, 
+// }, { timestamps: true });
+
+// export default mongoose.model("User", userSchema);
+
+
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  phone: { type: String, required: true }, 
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
